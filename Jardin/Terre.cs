c0bee8f860600c092fbee4f977c998be5e805a74
Fanélie <fbayle@ensc.fr>
2025-04-27 18:32:09 +0200

    changement

---
+++ Jardin/Terre.cs
@@ -0,0 +1,12 @@
+public class Terre : Terrain
+{
+    public Terre(int placeDisponible) : base (placeDisponible)
+    {
+
+    }
+    public override string ToString()
+    {
+        string affichage =" ";
+        return affichage;
+    }
+}
